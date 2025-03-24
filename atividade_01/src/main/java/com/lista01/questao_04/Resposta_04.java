e541394474801bffb3d954c0b2433630c96780f0
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-03-24 14:44:01 -0300

    feat: respondi a questão 04 do professor Régis

--- atividade_01/src/main/java/com/lista01/questao_04/Resposta_04.java
+++ atividade_01/src/main/java/com/lista01/questao_04/Resposta_04.java
@@ -1,7 +1,29 @@
 package com.lista01.questao_04;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Resposta_04 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o primeiro numero");
+        double primeiroNumero = scanner.nextDouble();
+
+        System.out.println("Digite o segundo numero");
+        double segundoNumero = scanner.nextDouble();
+
+        double valor1 = primeiroNumero + segundoNumero;
+        double valor2 = primeiroNumero - segundoNumero;
+        double valor3 = primeiroNumero * segundoNumero;
+        double valor4 = primeiroNumero / segundoNumero;
+        double valor5 = primeiroNumero % segundoNumero;
+
+        System.out.println("A soma dos valores é: "+valor1);
+        System.out.println("A subtração dos valores é: "+valor2);
+        System.out.println("A multiplicação dos valores é: "+valor3);
+        System.out.println("A divisão dos valores é: "+valor4);
+        System.out.println("A sobra da divisão é: "+valor5);
+        
     }
 }
