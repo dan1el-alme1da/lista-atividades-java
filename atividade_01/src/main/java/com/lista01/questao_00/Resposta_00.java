90aed663dcdb743d1ed741665295c2f7c786c15e
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-03-24 13:58:44 -0300

    feat: classes qustao 04 e questao 00

---
+++ atividade_01/src/main/java/com/lista01/questao_00/Resposta_00.java
@@ -0,0 +1,5 @@
+package com.lista01.questao_00;
+
+public class Resposta_00 {
+
+}
---
+++ atividade_01/src/main/java/com/lista01/questao_04/Resposta_04.java
@@ -0,0 +1,7 @@
+package com.lista01.questao_04;
+
+public class Resposta_04 {
+    public static void main(String[] args) {
+
+    }
+}
