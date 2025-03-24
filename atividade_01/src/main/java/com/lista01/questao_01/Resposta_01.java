348b92105b2ecb2c96c934a5216a5e079f7f3017
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-03-24 13:29:50 -0300

    feat: respondi a questao 1 e 2 do professor regis

---
+++ atividade_01/src/main/java/com/lista01/questao_01/Resposta_01.java
@@ -0,0 +1,8 @@
+package com.lista01.questao_01;
+
+public class Resposta_01 {
+    public static void main(String[] args) {
+        System.out.println("Daniel");
+        System.out.println("Salomao");
+    }
+}
