649714abacf8788bd6b139e0f3dbe1450f722c54
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-03-25 17:37:37 -0300

    feat: funcionalidades do/while para a divisão

--- atividade_01/src/main/java/com/lista01/questao_04/Resposta_04.java
+++ atividade_01/src/main/java/com/lista01/questao_04/Resposta_04.java
@@ -5,13 +5,28 @@ import java.util.Scanner;
 
 public class Resposta_04 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Digite o primeiro numero");
-        double primeiroNumero = scanner.nextDouble();
 
-        System.out.println("Digite o segundo numero");
-        double segundoNumero = scanner.nextDouble();
+
+        double primeiroNumero;
+        do {
+            System.out.println("Digite o primeiro numero (deve ser positivo e diferente de zero):");
+            primeiroNumero = scanner.nextDouble();
+            if (primeiroNumero <= 0) {
+                System.out.println("Erro: O primeiro número não pode ser zero ou negativo. Tente novamente.");
+            }
+        } while (primeiroNumero <= 0);
+
+
+        double segundoNumero;
+        do {
+            System.out.println("Digite o segundo numero (deve ser positivo e diferente de zero):");
+            segundoNumero = scanner.nextDouble();
+            if (segundoNumero <= 0) {
+                System.out.println("Erro: O segundo número não pode ser zero ou negativo. Tente novamente.");
+            }
+        } while (segundoNumero <= 0);
+
 
         double resultadoSoma = primeiroNumero + segundoNumero;
         double resultadoSubtracao = primeiroNumero - segundoNumero;
@@ -19,11 +34,11 @@ public class Resposta_04 {
         double resultadoDivisao = primeiroNumero / segundoNumero;
         double resultadoSobra = primeiroNumero % segundoNumero;
 
-        System.out.println("A soma dos valores é: "+resultadoSoma);
-        System.out.println("A subtração dos valores é: "+resultadoSubtracao);
-        System.out.println("A multiplicação dos valores é: "+resultadoMultiplicacao);
-        System.out.println("A divisão dos valores é: "+resultadoDivisao);
-        System.out.println("A sobra da divisão é: "+resultadoSobra);
-        
+
+        System.out.println("A soma dos valores é: " + resultadoSoma);
+        System.out.println("A subtração dos valores é: " + resultadoSubtracao);
+        System.out.println("A multiplicação dos valores é: " + resultadoMultiplicacao);
+        System.out.println("A divisão dos valores é: " + resultadoDivisao);
+        System.out.println("A sobra da divisão é: " + resultadoSobra);
     }
 }
